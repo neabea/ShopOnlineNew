318f2b8ae32c2bdff30f91c280e5f64be269a231
origin <https://github.com/neabea/ShopOnline.git>
2018-06-08 15:30:25 +0800

    完成广告轮播，登录数据库保存

--- example/src/main/java/sam/shopline/ExampleApp.java
+++ example/src/main/java/sam/shopline/ExampleApp.java
@@ -3,7 +3,12 @@ package sam.shopline;
 import android.app.Application;
 
 import com.example.latte_core.app.Latte;
+import com.example.latte_core.net.interceptors.DebugInterceptor;
+import com.example.latte_ec.database.DatabaseManager;
+import com.facebook.stetho.Stetho;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
 
 
 public class ExampleApp extends Application {
@@ -13,6 +18,20 @@ public class ExampleApp extends Application {
         Latte.init(this)
                 .withIcon(new FontAwesomeModule())
                 .withApiHost("http://127.0.0.1/")
+                .withInterceptor(new DebugInterceptor("index",R.raw.test))
                 .configure();
+        Logger.addLogAdapter(new AndroidLogAdapter());
+        initStetho();
+        DatabaseManager.getInstance().init(this);
     }
+
+
+        private void initStetho() {
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                        .build());
+    }
+
 }
