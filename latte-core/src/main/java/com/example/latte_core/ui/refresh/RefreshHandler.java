2649f082b63fbc2c11955b3d56ecfa8fadde7960
origin <https://github.com/neabea/ShopOnline.git>
2018-06-10 00:42:59 +0800

    完成首页json数据下载

--- latte-core/src/main/java/com/example/latte_core/ui/refresh/RefreshHandler.java
+++ latte-core/src/main/java/com/example/latte_core/ui/refresh/RefreshHandler.java
@@ -1,8 +1,12 @@
 package com.example.latte_core.ui.refresh;
 
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.Toast;
 
 import com.example.latte_core.app.Latte;
+import com.example.latte_core.net.RestClient;
+import com.example.latte_core.net.callback.ISuccess;
+import com.example.latte_core.util.log.LatteLogger;
 
 import java.util.logging.Handler;
 
@@ -15,6 +19,21 @@ public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener{
         REFRESH_LAYOUT.setOnRefreshListener(this);
     }
 
+    public void firstPage(String url) {
+        LatteLogger.d("IUDHAS", url);
+     //   BEAN.setDelayed(1000);
+        RestClient.builder()
+                .url(url)
+                .success(new ISuccess() {
+                    @Override
+                    public void onSucess(String response) {
+                        Toast.makeText(Latte.getApplicationContext(),response,Toast.LENGTH_LONG).show();
+                    }
+                })
+                .build()
+                .get();
+    }
+
     private void refresh() {
         REFRESH_LAYOUT.setRefreshing(true);
         Latte.getHandler().postDelayed(new Runnable() {
@@ -24,8 +43,6 @@ public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener{
                 REFRESH_LAYOUT.setRefreshing(false);//关闭下拉动画
             }
         },2000);
-
-
     }
 
     @Override
